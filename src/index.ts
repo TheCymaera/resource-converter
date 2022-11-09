f283e508f1c8811e3496c7e0fc921ea3bf449ea5
TheCymaera <69355340+TheCymaera@users.noreply.github.com>
2022-11-09 19:07:12 +0800

    🎨 Renamed files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * as asyncGenerator from "./asyncGenerator.js";
-export * as blob from "./blobs.js";
+export * as blob from "./blob.js";
 export * as dataURL from "./dataURL.js";
 export * as generator from "./generator.js";
-export * as image from "./images.js";
+export * as image from "./image.js";
 export * as readableStream from "./readableStream.js";
